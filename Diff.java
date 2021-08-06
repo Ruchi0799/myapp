2ded6f205445782c38f2b211e366f9441f796131
Ruchi0799 <88529533+Ruchi0799@users.noreply.github.com>
2021-08-06 16:46:36 +0530

    update diff in remote

--- Diff.java
+++ Diff.java
@@ -1,7 +1,7 @@
 public class Diff {
    public static void main(String[] args)
     {
-        System.out.println("Difference is this");
+        System.out.println("Difference is 5-4");
 	
     }
 }
