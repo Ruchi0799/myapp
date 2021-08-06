f8345a343ad2b7aff846a7476e8b74312644fb3a
Ruchi Mandve <mandve.ruchi.17ce8002@gmail.com>
2021-08-06 15:30:40 +0530

    changes HelloRuchi.java

--- HelloRuchi.java
+++ HelloRuchi.java
@@ -2,5 +2,6 @@ public class HelloRuchi {
    public static void main(String[] args)
     {
         System.out.println("Hello World! Its me Ruchi!");
+	System.out.println("Hello World! its PRACHI");
     }
 }
